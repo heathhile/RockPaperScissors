e16b64e709a6cc8e4e4ba595799df8fba50b34dd
Heath Hile <heathhile@gmail.com>
2023-06-08 23:01:02 -0400

    randoms thing is working, now to fix normal one

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 //  Variables
 // const PlayerChoice = prompt("Make a selection: ", "Rock, Paper, or Scissors?").toUpperCase();    
-const ComputerChoice = getComputerChoice().toUpperCase();
-const PlayerChoice = getPlayerChoice().toUpperCase();
+let ComputerChoice = getComputerChoice().toUpperCase();
+let PlayerChoice = getPlayerChoice().toUpperCase();
 
 let playerScore = 0;
 let computerScore = 0;
@@ -76,20 +76,44 @@ function playRound(PlayerChoice, ComputerChoice) {
     }
 }
 
-function game() {
-    for (let i= 1; i < 6; i++) {
-        playRound(PlayerChoice, ComputerChoice);
-        // testing my looping >.<
-        // console.log(i);
+function testingRandomNums() {
+    for (let i = 0; i < 10; i++) {
+        let randomNumber1 = Math.floor(Math.random() * 5);
+        let randomNumber2 = Math.floor(Math.random() * 5);
+
+        if (randomNumber1 > randomNumber2) {
+            playerScore++;
+        }
+        else {
+            computerScore++;
+        }
     }
     if (playerScore > computerScore) {
         console.log("Player wins with " + playerScore);
+        console.log("Computer had " + computerScore);
+
     }
-    else if (playerScore < computerScore) {
+    else {
         console.log("Computer wins with " + computerScore);
+        console.log("Player had " + playerScore);
     }
 }
 
+// function game() {
+//     for (let i= 1; i < 6; i++) {
+//         playRound(PlayerChoice, ComputerChoice);
+//         // testing my looping >.<
+//         // console.log(i);
+//     }
+//     if (playerScore > computerScore) {
+//         console.log("Player wins with " + playerScore);
+//     }
+//     else if (playerScore < computerScore) {
+//         console.log("Computer wins with " + computerScore);
+//     }
+// }
+
 // console.log(ComputerChoice)
 // console.log(playRound(PlayerChoice, ComputerChoice));
-game();
+// game();
+testingRandomNums();
