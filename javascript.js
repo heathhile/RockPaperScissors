b82f7fa1c3b39b9cb5b915a95c01dfc0174e65ba
Heath Hile <heathhile@gmail.com>
2023-06-08 22:01:52 -0400

    kinda working if statement

--- javascript.js
+++ javascript.js
@@ -1,25 +1,45 @@
+//  Variables
+const PlayerChoice = prompt("Make a selection: ", "Rock, Paper, or Scissors?").toUpperCase();
+const ComputerChoice = getComputerChoice().toUpperCase();
+
+
 function getComputerChoice() {
     let choice = ""
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
-        choice = "Rock";
+        choice = "ROCK";
     }
     else if (randomNumber === 1) {
-        choice = "Paper";
+        choice = "PAPER";
     }
     else {
-        choice = "Scissors";
+        choice = "SCISSORS";
     }
 
     return choice;
 }
 
+// Single round between player and computer
 function singleRound(playerSelection, computerSelection) {
-    playerSelection
-    
+    if (PlayerChoice === ComputerChoice) {
+        console.log(`"Player got ${PlayerChoice}`);
+        console.log(`"Computer gpt ${ComputerChoice}"`);
+        console.log("You tied! Try again!");
+    }
+    else if ((PlayerChoice === "ROCK") && (ComputerChoice === "SCISSORS")) {
+        console.log(`"Player got ${PlayerChoice}`);
+        console.log(`"Computer gpt ${ComputerChoice}"`);
+        console.log(`"You win with ${PlayerChoice}"`);
+    }
+    else if ((PlayerChoice === "SCISSORS") && (ComputerChoice === "PAPER")) {
+        console.log(`"Player got ${PlayerChoice}`);
+        console.log(`"Computer gpt ${ComputerChoice}"`);
+        console.log(`"You win with ${PlayerChoice}"`);
+    }
+    else {
+        console.log("Yay!");
+    }
 }
 
-const playerSelection = prompt("Make a selection: ", "Rock, Paper, or Scissors?").toUpperCase;
-const ComputerChoice = getComputerChoice();
-
-console.log(singleRound(playerSelection, computerSelection));
+// console.log(ComputerChoice)
+console.log(singleRound(PlayerChoice, ComputerChoice));
