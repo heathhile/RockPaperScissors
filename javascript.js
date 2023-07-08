0c43967c9adbd10442e49e4908017a06fbb15606
Heath Hile <heathhile@gmail.com>
2023-07-07 21:58:40 -0400

    looks like i meet all TOP conditions

--- javascript.js
+++ javascript.js
@@ -1,54 +1,23 @@
 //  Variables
-// const PlayerChoice = prompt("Make a selection: ", "Rock, Paper, or Scissors?").toUpperCase();    
+let choices = ['rock', 'paper', 'scissors']    
 let ComputerChoice = getComputerChoice().toUpperCase();
-let PlayerChoice = getPlayerChoice().toUpperCase();
-
 let playerScore = 0;
 let computerScore = 0;
-
+let tiedScore = 0;
 
 // Choose your weapon!!
 function getComputerChoice() {
-    let choice = ""
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-        choice = "ROCK";
-    }
-    else if (randomNumber === 1) {
-        choice = "PAPER";
-    }
-    else {
-        choice = "SCISSORS";
-    }
-
-    return choice;
-}
-
-// Choose your weapon!!
-function getPlayerChoice() {
-    let choice = ""
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-        choice = "PAPER";
-    }
-    else if (randomNumber === 1) {
-        choice = "SCISSORS";
-    }
-    else {
-        choice = "ROCK";
-    }
-
-    return choice;
+    return choices[Math.floor(Math.random() * choices.length)];;
 }
 
 // Single round between player and computer
 function playRound(PlayerChoice, ComputerChoice) {
 
-
     if (PlayerChoice === ComputerChoice) {
         console.log(`"Player got ${PlayerChoice}`);
         console.log(`"Computer gpt ${ComputerChoice}"`);
         console.log("You tied! Try again!");
+        tiedScore++;
     }
     else if ((PlayerChoice === "ROCK") && (ComputerChoice === "SCISSORS")) {
         console.log(`"Player got ${PlayerChoice}`);
@@ -75,45 +44,26 @@ function playRound(PlayerChoice, ComputerChoice) {
         computerScore++;
     }
 }
-
-function testingRandomNums() {
-    for (let i = 0; i < 10; i++) {
-        let randomNumber1 = Math.floor(Math.random() * 5);
-        let randomNumber2 = Math.floor(Math.random() * 5);
-
-        if (randomNumber1 > randomNumber2) {
-            playerScore++;
-        }
-        else {
-            computerScore++;
-        }
+function game() {
+    // Loops through and gets the players choice
+    // then gets computer choice and plays Thermonuclear War
+    for (let i = 0; i < 5; i++) {     
+        const PlayerChoice = prompt("Make a selection: ", "Rock, Paper, or Scissors?").toUpperCase();
+        playRound(PlayerChoice, ComputerChoice);
     }
+
+    //Compares and outputs the score.
     if (playerScore > computerScore) {
         console.log("Player wins with " + playerScore);
         console.log("Computer had " + computerScore);
+        console.log("You tied " + tiedScore + " times!")
 
     }
     else {
         console.log("Computer wins with " + computerScore);
         console.log("Player had " + playerScore);
+        console.log("You tied " + tiedScore + " times!")
     }
 }
+game();
 
-// function game() {
-//     for (let i= 1; i < 6; i++) {
-//         playRound(PlayerChoice, ComputerChoice);
-//         // testing my looping >.<
-//         // console.log(i);
-//     }
-//     if (playerScore > computerScore) {
-//         console.log("Player wins with " + playerScore);
-//     }
-//     else if (playerScore < computerScore) {
-//         console.log("Computer wins with " + computerScore);
-//     }
-// }
-
-// console.log(ComputerChoice)
-// console.log(playRound(PlayerChoice, ComputerChoice));
-// game();
-testingRandomNums();
