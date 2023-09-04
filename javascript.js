c0d42e1ef3d91e5db26cbea2f7ab1f7ddfa1dc03
Heath Hile <heathhile@gmail.com>
2023-09-03 20:15:40 -0400

    trying tog et variable to write to score div

--- javascript.js
+++ javascript.js
@@ -1,15 +1,17 @@
 //  Variables
-let choices = ['rock', 'paper', 'scissors']    
-let ComputerChoice = getComputerChoice().toUpperCase();
+let choices = ['ROCK!', 'PAPER!', 'SCISSORS!']    
 let playerScore = 0;
 let computerScore = 0;
 let tiedScore = 0;
 //
 // Choose your weapon!!
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];;
+    return choices[Math.floor(Math.random() * 3)];;
 }
 
+function update_score(playerScore) {
+
+}
 
 
 // Single round between player and computer
@@ -21,19 +23,19 @@ function playRound(PlayerChoice, ComputerChoice) {
         console.log("You tied! Try again!");
         tiedScore++;
     }
-    else if ((PlayerChoice === "ROCK") && (ComputerChoice === "SCISSORS")) {
+    else if ((PlayerChoice === "ROCK!") && (ComputerChoice === "SCISSORS!")) {
         console.log(`"Player got ${PlayerChoice}`);
         console.log(`"Computer gpt ${ComputerChoice}"`);
         console.log(`"You win with ${PlayerChoice}"`);
         playerScore++;
     }
-    else if ((PlayerChoice === "SCISSORS") && (ComputerChoice === "PAPER")) {
+    else if ((PlayerChoice === "SCISSORS!") && (ComputerChoice === "PAPER!")) {
         console.log(`"Player got ${PlayerChoice}`);
         console.log(`"Computer gpt ${ComputerChoice}"`);
         console.log(`"You win with ${PlayerChoice}"`);
         playerScore++;
     }
-    else if ((PlayerChoice === "PAPER") && (ComputerChoice === "ROCK")) {
+    else if ((PlayerChoice === "PAPER!") && (ComputerChoice === "ROCK!")) {
         console.log(`"Player got ${PlayerChoice}`);
         console.log(`"Computer gpt ${ComputerChoice}"`);
         console.log(`"You win with ${PlayerChoice}"`);
@@ -48,35 +50,36 @@ function playRound(PlayerChoice, ComputerChoice) {
 }
 
 const btns = document.querySelectorAll('[data-button]');
+let btnsChoice = undefined;
 
 btns.forEach(button => {
     button.addEventListener('click', () => {
         console.log(button.innerText);
+        btnsChoice = button.innerText;
+        game(btnsChoice)
     })
 })
 
-function game() {
-    // Loops through and gets the players choice
-    // then gets computer choice and plays Thermonuclear War
-    // !!!! Commented per assignement
-    // for (let i = 0; i < 5; i++) {     
-    //     const PlayerChoice = prompt("Make a selection: ", "Rock, Paper, or Scissors?").toUpperCase();
-    //     playRound(PlayerChoice, ComputerChoice);
-    // }
+function game(PlayerChoice) {
+    let ComputerChoice = getComputerChoice().toUpperCase();
+    // let p_score = document.querySelector('[data-player-score]')
+    playRound(PlayerChoice, ComputerChoice);
 
     //Compares and outputs the score.
 
-    // if (playerScore > computerScore) {
-    //     console.log("Player wins with " + playerScore);
-    //     console.log("Computer had " + computerScore);
-    //     console.log("You tied " + tiedScore + " times!")
+    if (playerScore > computerScore) {
+        document.getElementById("player_score").innerHTML = playerScore.toString;
+        console.log("Player wins with " + playerScore);
+        console.log("Computer had " + computerScore);
+        console.log("You tied " + tiedScore + " times!")
 
-    // }
-    // else {
-    //     console.log("Computer wins with " + computerScore);
-    //     console.log("Player had " + playerScore);
-    //     console.log("You tied " + tiedScore + " times!")
-    // }
+    }
+    else {
+        console.log("Computer wins with " + computerScore);
+        console.log("Player had " + playerScore);
+        console.log("You tied " + tiedScore + " times!")
+    }
+
 }
 game();
 
